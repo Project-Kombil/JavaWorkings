84c112aa1929333972754ac05a4271ccd929c03b
Your-M4tu <alefay.bunes@gmail.com>
2020-09-21 16:26:43 +1000

    Updated File

---
+++ Your-M4tu/src/Capital.java
@@ -0,0 +1,9 @@
+public class Capital {
+    public static void main(String[] args){
+    System.out.println("Hello Kate");
+    int distance = 0;
+    System.out.println("The value is "+distance);
+        distance = 40;
+        System.out.println("The value changed to "+distance);
+    }
+}
