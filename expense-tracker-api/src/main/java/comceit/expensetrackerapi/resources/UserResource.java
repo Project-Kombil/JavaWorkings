6f555b0a72d78a96f95fc4d720074a3217e9f870
lab1pc3 <lab1pc3@localhost.localdomain>
2020-11-05 16:26:15 +1000

    Thursday

--- expense-tracker-api/src/main/java/comceit/expensetrackerapi/resources/UserResource.java
+++ expense-tracker-api/src/main/java/comceit/expensetrackerapi/resources/UserResource.java
@@ -1,24 +1,34 @@
 package comceit.expensetrackerapi.resources;
 
+import comceit.expensetrackerapi.domains.User;
+import comceit.expensetrackerapi.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserResource {
-
+    @Autowired
+    UserService userService;
     @PostMapping("/register")
-    public String registerUser(@RequestBody Map<String,String> userMap){
+    public ResponseEntity<Map<String,String>> registerUser(@RequestBody Map<String,String> userMap){
         String firstName = (String) userMap.get("firstName");
         String lastName = (String) userMap.get("lastName");
         String email = (String) userMap.get("email");
         String password = (String) userMap.get("password");
-
-        return firstName + "," + email;
+        User user = userService.registerUser(firstName,lastName,email,password);
+        Map<String,String> response = new HashMap<>();
+        response.put("Status","1");
+        response.put("Message","User Created Successfully");
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
 }
---
+++ expense-tracker-api/src/main/java/comceit/expensetrackerapi/services/UserServiceImp.java
@@ -0,0 +1,39 @@
+package comceit.expensetrackerapi.services;
+
+import comceit.expensetrackerapi.domains.User;
+import comceit.expensetrackerapi.exceptions.EAuthExceptions;
+import comceit.expensetrackerapi.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.regex.Pattern;
+@Service
+@Transactional
+public class UserServiceImp implements UserService{
+    @Autowired
+    UserRepository userRepository;
+    @Override
+    public User validateUser(String email, String password) throws EAuthExceptions {
+        return null;
+    }
+
+    @Override
+    public User registerUser(String firstName, String lastName, String email, String password) throws EAuthExceptions {
+        Pattern pattern = Pattern.compile("^(.+)@(.+)$");
+        if(email != null){
+            email = email.toLowerCase();
+            //Checking if the email in correct pattern
+            if(!pattern.matcher(email).matches())
+                throw new EAuthExceptions("Invalid Email Format");
+            //Checking if the email already exists
+            Integer count = userRepository.getCountByEmail(email);
+            if(count > 0) throw new EAuthExceptions("Email Already in Use");
+            //We can register the user using the create api
+            Integer userId = userRepository.create(firstName,lastName,email,password);
+            //return the User object from the database
+            return userRepository.findById(userId);
+        }
+        return null;
+    }
+}
